21945026c59554a0ee1b7dbbfc4e5d414db32ea3
isaiahjordan <33890029+isaiahjordan@users.noreply.github.com>
2017-11-21 22:53:58 -0800

    Basic model functioning
    For loop is in progress, not yet added.

---
+++ bpfbio.R
@@ -0,0 +1,35 @@
+library(deSolve)
+library(lme4)
+#ampicillin concentration and position
+ampconc <- seq(0, 1, by = 0.01)
+ampco <- rev((exp(ampconc) / 2.72))
+#for (a in ampco) {
+  parameters = c(
+    ampc = 0.8,
+    trate = 1.5,
+    degtc = 1,
+    synrate = 1.5
+    #tet=0.5
+  )
+  #ode model of tetC levels and tet level in the cell
+  state <- c(tet = 0.5,
+             tetc = .025)
+  
+  tetconc <- function(t, state, parameters) {
+    with(as.list(c(state, parameters)), {
+      dtet = (tet) - (trate * tetc) * (tet) / (0.5 + tet)
+      dtetc = synrate * ampc - (degtc * tetc)
+      list(c(dtet, dtetc))
+    })
+  }
+#}
+out <-
+  as.data.frame(ode(
+    y = state,
+    times = seq(1, 50),
+    func = tetconc,
+    parms = parameters,
+    #method = "lsoda"
+  ))
+tail(out)
+plot(x = out$time, y = out$tet,xlim=c(0,10))
